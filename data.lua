44d1154984612f191915337a9fb68eafd32541ea
Wyrrrd <wyrrrd@gmx.de>
2020-02-20 12:24:11 +0100

    Fixed wrong type check
    Checked for item instead of recipe

--- data.lua
+++ data.lua
@@ -1,12 +1,12 @@
-if data.raw.item["speed-module-requestor"] then
+if data.raw.recipe["speed-module-requestor"] then
   data.raw.recipe["speed-module-requestor"].ingredients = {}
 end
-if data.raw.item["efficiency-module-requestor"] then
+if data.raw.recipe["efficiency-module-requestor"] then
   data.raw.recipe["efficiency-module-requestor"].ingredients = {}
 end
-if data.raw.item["productivity-module-requestor"] then
+if data.raw.recipe["productivity-module-requestor"] then
   data.raw.recipe["productivity-module-requestor"].ingredients = {}
 end
-if data.raw.item["custom-module-requestor"] then
+if data.raw.recipe["custom-module-requestor"] then
   data.raw.recipe["custom-module-requestor"].ingredients = {}
 end
